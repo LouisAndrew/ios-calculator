df4399253cf286e62f65598c3eed5c23767edda5
Louis Andrew <louisandrew3@gmail.com>
2021-11-11 16:49:34 +0100

    feat: initiate UI for the top half

--- calculator/Constants/AppColors.swift
+++ calculator/Constants/AppColors.swift
@@ -11,4 +11,5 @@ import SwiftUI
 struct AppColors {
  static let primary = Color.init(red: 22/255, green: 48/255, blue: 43/255)  
  static let secondary = Color.init(red: 13/255, green: 28/255, blue: 25/255)
+ static let text = Color.init(red: 192/255, green: 229/255, blue: 200/255)
 }
--- calculator/Views/TopHalf.swift
+++ calculator/Views/TopHalf.swift
@@ -8,11 +8,32 @@
 import SwiftUI
 
 struct TopHalf: View {
+
+    @State var value: String = "##"
+    @State var operations: [String] = ["12", "+", "324"]
+
     var primaryColor: Color
     var body: some View {
-        VStack {}
-            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
-            .background(primaryColor)
+        VStack {
+            VStack {
+                Text(join(self.operations))
+                    .fontWeight(.bold)
+                    .font(.system(size: 24))
+                    .foregroundColor(AppColors.text)
+                    .padding(.trailing, 16)
+                    .padding(.bottom, 2)
+
+                Text(self.value)
+                    .fontWeight(.light)
+                    .font(.system(size: 64))
+                    .foregroundColor(.white)
+            }
+            .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .bottomTrailing)
+            .padding(.bottom, 32)
+            .padding(.trailing, 32)
+        }
+        .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: .infinity, alignment: .topLeading)
+        .background(primaryColor)
     }
 }
 
@@ -20,4 +41,8 @@ struct TopHalf_Previews: PreviewProvider {
     static var previews: some View {
         TopHalf(primaryColor: AppColors.primary)
     }
+}
+
+func join(_ array: [String]) -> String {
+    return array.joined(separator: " ")
 }
